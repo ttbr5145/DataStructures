a43f4be0bc22601baf8eef3968fd23591a43435c
吴桐 <105585207+ttbr5145@users.noreply.github.com>
2024-08-03 23:03:55 +0800

    完成skiplist的初始化，插入，查找，调试输出模块，但无法正常运行

--- skiplist.hpp
+++ skiplist.hpp
@@ -11,6 +11,11 @@ using namespace std;
 using namespace List;
 namespace List
 {
+    //用于生成随机数
+	default_random_engine rd;
+    bernoulli_distribution dist(0.5);// 生成1的概率为0.5
+    const int MAXN = 20010;
+
     template <class Data>
     class skiplist
     {
@@ -39,41 +44,84 @@ namespace List
                         prev = pre, next = nxt, child = chd, father = fa;
                     }
             };
-            Node *head;    //head永远为(0,NULL,next,NULL,NULL)
+            Node *head[MAXN], *tail[MAXN];
             int length;
             int maxlevel;
 
-            //用于生成随机数
-	        default_random_engine e;
-            bernoulli_distribution u(0.5); // 生成1的概率为0.5
             skiplist()
             {
                 length = 0;
                 maxlevel = 0;
-                head = new Node(0, NULL, NULL, NULL, NULL);
+                _for(i, 0, MAXN)
+                {
+                    head[i] = new Node(0);
+                    tail[i] = new Node(0);
+                    head[i]->next = tail[i];
+                    tail[i]->prev = head[i];
+                }
 
                 //初始化随机数
-                e.seed(time(0));
+                rd.seed(time(0));
             }
-            skiplist(Data val)
+            /*skiplist(Data val)
             {
                 length = 1;
                 maxlevel = 0;
-                head = new Node(0, NULL, new Node(val), NULL, NULL);
-                head->next->prev = head;
+                _for(i, 0, MAXN)
+                {
+                    head[i] = new Node(0)
+                    tail[i] = new Node(0);
+                    head[i]->next = tail[i];
+                    tail[i]->prev = head[i];
+                }
+                head[0]->next = new Node(val);
+                head[0]->next->next = tail[0], tail[0]->prev = head[0]->next;
+                head[0]->next->prev = head[0];
 
                 //初始化随机数
-                e.seed(time(0));
-            }
+                rd.seed(time(0));
+            }*/
 
             int randlevel()//获取随机层数
             {
                 return randlevel(0);
             }
+
+            void insert(Data val)//插入数据
+            {
+                int newlevel = randlevel();
+                if (newlevel > maxlevel)
+                {
+                    newlevel = maxlevel + 1;
+                    head[maxlevel + 1]->child = head[maxlevel];
+                    head[maxlevel]->father = head[maxlevel + 1];
+                    tail[maxlevel +1]->child = tail[maxlevel];
+                    tail[maxlevel]->father = tail[maxlevel];
+                    maxlevel++;
+                }
+                insert(newlevel, NULL, head[newlevel], tail[newlevel], val);
+            }
+            Node* find(Data val)//搜索数据（最高层）
+            {
+                return find(maxlevel, head[maxlevel], tail[maxlevel], val);
+            }
+            void del(Data val);
+            
+            void print()
+            {
+                int nowlevel = maxlevel;
+                for (Node* start = head[maxlevel]; start != NULL; start = start->child, nowlevel--)
+                {
+                    printf("level %d: ", nowlevel);
+                    for (Node* p = start->next; p->next != NULL; p = p->next)
+                        printf("%d ",p->value);
+                    printf("\n");
+                }
+            }
         private:
             bool rand()//随机生成0或1
             {
-                return u(e);
+                return dist(rd);
             }
             int randlevel(int level)//获得随机层数，底层
             {
@@ -89,5 +137,46 @@ namespace List
                 }
             }
             
+            Node* insert(int level, Node* fa, Node* head, Node* tail, Data val)
+            {
+                Node *left = head, *right = tail;
+                while (left->value < val) left = left->next;
+                while (right->value > val) right = right->prev;
+                if (left->next == right)
+                {
+                    Node *node = left->next = new Node(val, left->child, right->child, fa, NULL);
+                    right->prev = node;
+                    if (level > 0)
+                        node->child = insert(level - 1, node, left, right, val);
+                    else
+                        node->child = NULL;
+                    return node;
+                }
+                else//左右指针重合，均指在val处
+                {
+                    //left->times++;
+                    insert(level - 1, left, left, right, val);
+                    return left;
+                }
+            }
+            Node* find(int level, Node* head, Node* tail, int val)
+            {
+                Node *left = head, *right = tail;
+                while (left->value < val) left = left->next;
+                while (right->value > val) right = right->prev;
+                if (left->next == right)
+                {
+                    if(level > 0)
+                        return find(level - 1, left->child, right->child);
+                    else
+                        return NULL;
+                }
+                else
+                {
+                    return left;
+                }
+            }
+            void mov_after(Node* node);
+            void mov_before(Node* node);
     };
 }
--- test.cpp
+++ test.cpp
@@ -1,19 +1,18 @@
 #include<bits/stdc++.h>
-#include<iostream>
-#include<random>
-#include<ctime>
+#include"skiplist.hpp"
 #define _for(i,a,b) for(int i = a; i <= b; i++)
 #define _rfor(i,a,b) for(int i=a; i>=b; i--)
 using namespace std;
-default_random_engine e;
-bernoulli_distribution u(0.5); // 随机结果为（0或）1的概率（各）为0.5
-void rrand()
-{
-    cout<<u(e)<<' ';
-}
+using namespace List;
 int main()
 {
-    e.seed(time(0));
-	_for(i,1,10) rrand();
+    skiplist<int> sl;
+    _for(i,1,10)
+    {
+        int a;
+        cin>>a;
+        sl.insert(a);
+    }
+    sl.print();
     return 0;
 }

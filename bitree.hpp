15d5ffd43ec82ed59fb4ed924cbacc19843e2e90
CaiHeran <Tsai.love.dev@outlook.com>
2024-08-08 15:44:50 +0800

    调整代码格式

--- bitree.hpp
+++ bitree.hpp
@@ -1,5 +1,3 @@
-#include<cstdio>
-using namespace std;
 #ifdef NULL
 #undef NULL
 #endif
@@ -15,29 +13,18 @@ namespace Tree
         {
         public:
             Data value;
-            Node *father, *lftchild, *rgtchild;
+            Node *father=nullptr, *lftchild=nullptr, *rgtchild=nullptr;
             bool _at;//0表示在左子树，否则在右子树
             int degree() { return (lftchild != nullptr) + (rgtchild != nullptr); }
-            Node()
-            {
-                father = lftchild = rgtchild = nullptr;
-            }
-            Node(Data val)
-            {
-                value = val;
-                father = lftchild = rgtchild = nullptr;
-            }
+            Node() {}
+            Node(Data val) : value(val)
+            {}
             Node(Data val, Node* fa)
-            {
-                value = val;
-                father = fa;
-                lftchild = rgtchild = nullptr;
-            }
+              : value(val), father(fa)
+            {}
             Node(Data val, Node* fa, Node* lft, Node* rgt)
-            {
-                value = val;
-                father = fa, lftchild = lft, rgtchild = rgt;
-            }
+              : value(val), father(fa), lftchild(lft), rgtchild(rgt)
+            {}
         };
         Node* root;
         
@@ -45,23 +32,29 @@ namespace Tree
         bool insert_able(Node* fa) { return fa->degree() < 2; }  //查询是否可以插入结点
         void insert_at(Node* fa, Data val)
         {
-            if (fa->lftchild == nullptr) fa->lftchild = new Node(val, fa);
-            else                      fa->rgtchild = new Node(val, fa);
+            if (!fa->lftchild)
+                fa->lftchild = new Node(val, fa);
+            else
+                fa->rgtchild = new Node(val, fa);
         }
         void del_at(Node* root)
         {
-            if (root == nullptr) return;
-            if (root->lftchild != nullptr) del(root->lftchild);
-            if (root->rgtchild != nullptr) del(root->rgtchild);
-            if (root->_at) root->father->rhtchild = nullptr;
-            else           root->father->lftchild = nullptr;
+            if (!root) return;
+            if (root->lftchild)
+                del(root->lftchild);
+            if (root->rgtchild)
+                del(root->rgtchild);
+            if (root->_at)
+                root->father->rhtchild = nullptr;
+            else
+                root->father->lftchild = nullptr;
             delete root;
         }
         void print() { print(root); }         //中序遍历输出
     private:
         void print(Node* node)
         {
-            if (node==nullptr) return;
+            if (!node) return;
             printf("%d at %s\n", node->value, (node->_at ? "rht" : "lft") );
             print(node->lftchild);
             print(node->rgtchild);
